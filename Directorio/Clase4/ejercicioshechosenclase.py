682e32b9aa4a707135b5562f07744428dbe7d040
pikdisho <yapurafrancodavid@gmail.com>
2022-08-27 21:11:30 +0200

    enclase

--- Directorio/Clase4/ejercicioshechosenclase.py
+++ Directorio/Clase4/ejercicioshechosenclase.py
@@ -1,3 +1,17 @@
 # Crea un programa que pida al usuario un número de mes (por ej el 4) y diga cuantos dias tiene (por ej 30) y el nombre del mes.
 # Para simplificarlo vamos a suponer que febrero tiene 28 dias. Usar diccionarios
 
+from logging.config import dictConfig
+
+
+meselegido = int(input('Ingrese un número de mes: '))
+diccionario_de_meses = {
+    'meses': ['Enero', 'Febrero', 'Marzo'],
+    'duraciondelmes': [31,28,30]
+
+    }
+    
+
+}
+
+print('El mes elegido tiene', diccionario_de_meses)
