d24810ab55948f688a821896b4290302019b82f3
pikdisho <yapurafrancodavid@gmail.com>
2022-08-27 20:50:24 +0200

    new file:   Directorio/Clase4/ejercicioshechosenclase.py

---
+++ Directorio/Clase4/ejercicioshechosenclase.py
@@ -0,0 +1,3 @@
+# Crea un programa que pida al usuario un número de mes (por ej el 4) y diga cuantos dias tiene (por ej 30) y el nombre del mes.
+# Para simplificarlo vamos a suponer que febrero tiene 28 dias. Usar diccionarios
+
