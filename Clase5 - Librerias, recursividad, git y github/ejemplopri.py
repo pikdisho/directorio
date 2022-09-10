b1521d4a128396c50e5367d65970475f00317ac1
pikdisho <yapurafrancodavidq@gmail.com>
2022-09-10 18:16:24 -0300

    .

---
+++ Clase5 - Librerias, recursividad, git y github/ejemplopri.py
@@ -0,0 +1,13 @@
+
+multiplicador = 1
+si apretamos pulsadorparasumar:
+if pulsadorparasumar = 1
+    multiplicador = multiplicador + 1
+
+semafororojo = 3 * multiplicador
+semaforoamarillo = 1 * multiplicador
+semaforoverde = 3 * multiplicador
+
+print(semafororojo)
+print(semaforoamarillo)
+print(semaforoverde)
