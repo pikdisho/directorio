18de38a667d2844d615e45731e9293bdec75c888
pikdisho <yapurafrancodavid@gmail.com>
2022-09-20 10:42:07 -0300

    .

--- Proyecto/core/settings.py
+++ Proyecto/core/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env
+environ.Env.read_env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent #→ donde se encuentra el directorio
@@ -21,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent #→ donde se encuentra el dir
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@3&4ru_94x50k&2#6(u$z0r$hv^ei9pqe215y+lh*fz7^f+7pn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*'] #→ para allowear todo
 
