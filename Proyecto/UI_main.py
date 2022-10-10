17d95b456dd9e1ae0f678734b9682d8708094695
pikdisho <yapurafrancodavid@gmail.com>
2022-10-10 04:43:12 -0300

    pikwIdget main progress

--- Proyecto/UI_main.py
+++ Proyecto/UI_main.py
@@ -1,35 +1,50 @@
 import tkinter
+from turtle import bgcolor
 from PIL import ImageTk, Image
 import functions
+from clases import *
+
 timer_refresco=1000
 
-class preferences():    
-    def __init__(self):
-        with open(r'Proyecto\preferences\nombre_usuario.txt', 'r') as archivo_nombre:
-            self.nombre = archivo_nombre.readline()
-        with open(r'Proyecto\preferences\resolucion_usuario.txt', 'r') as archivo_resolucion:
-            self.resolucion = archivo_resolucion.readline()
-    def get_nombre(self):
-        return(self.nombre)
-    def get_resolucion(self):
-        return(self.resolucion)
-    def get_ancho_alto(self):
-        ancho = int(self.resolucion[0:4]) # PARA PODER REDIMENSIONAR EL TAMAñO DE LOS FRAMES E IMAGENES
-        alto = int(self.resolucion[5:9])
-        return(ancho, alto)
-
-user_preferences = preferences()
+user_preferences = import_user_data()
 ancho, alto = user_preferences.get_ancho_alto()
 
-class establecer_preferencias_ventana():
-    def __init__(self, ventana_a_crear):
-        ventana_a_crear.geometry(user_preferences.get_resolucion())
-        ventana_a_crear.title('pikWidget')
-        ventana_a_crear.resizable(0,0)
-        ventana_a_crear.iconbitmap('wm', r'Proyecto\media\favicon.ico')
-
 main_window = tkinter.Tk()
-establecer_preferencias = establecer_preferencias_ventana(main_window)
+establecer_preferencias = user_preferences.set_main_window_preferences(main_window)
+
+# frame_logo = tkinter.Frame(main_window)
+# tamano_frame_logo = ConfigurarFrame(frame_logo, 15, 10)
+# frame_logo.config(bg='red')
+# frame_logo.pack_propagate(False)
+# frame_logo.pack()
+
+
+frame_barra = tkinter.Frame(main_window)
+tamano_barra = ConfigurarFrame(frame_barra, 15,100)
+frame_barra.config(bg='green')
+frame_barra.pack_propagate(False)
+frame_barra.pack(side=tkinter.LEFT)
+
+imagen_logo = Image.open(r'media\frame_logo.png')
+imagen_logo = imagen_logo.resize((int(ancho*0.15), int(alto*0.1)), Image.Resampling.LANCZOS)
+imagennn = ImageTk.PhotoImage(imagen_logo)
+imagen_label = tkinter.Label(frame_barra, image=imagennn)
+imagen_label.config(bg='#6d407f')
+imagen_label.pack()
+
+
+frame_reloj = tkinter.Frame(main_window)
+tamano_reloj = ConfigurarFrame(frame_reloj,85,10)
+
+frame_reloj.config(bg='blue')
+frame_reloj.pack_propagate(False)
+frame_reloj.pack()
+
+saludo = str('!Buenos dias, ' + user_preferences.get_nombre())
+saludo_variable = tkinter.StringVar(frame_reloj,value=saludo)
+
+saludo_label = tkinter.Label(main_window, textvariable=saludo_variable, font='Consolas 40')
+saludo_label.pack()
 main_window.mainloop()
 
 
--- Proyecto/clases.py
+++ Proyecto/clases.py
@@ -1,8 +1,8 @@
-class preferences():    
+class import_user_data():    
     def __init__(self):
-        with open(r'Proyecto\preferences\nombre_usuario.txt', 'r') as archivo_nombre:
+        with open(r'preferences\nombre_usuario.txt', 'r') as archivo_nombre:
             self.nombre = archivo_nombre.readline()
-        with open(r'Proyecto\preferences\resolucion_usuario.txt', 'r') as archivo_resolucion:
+        with open(r'preferences\resolucion_usuario.txt', 'r') as archivo_resolucion:
             self.resolucion = archivo_resolucion.readline()
     def get_nombre(self):
         return(self.nombre)
@@ -12,10 +12,17 @@ class preferences():
         ancho = int(self.resolucion[0:4]) # PARA PODER REDIMENSIONAR EL TAMAñO DE LOS FRAMES E IMAGENES
         alto = int(self.resolucion[5:9])
         return(ancho, alto)
+    def set_main_window_preferences(self, ventana_a_configurar):
+        ventana_a_configurar.geometry(self.get_resolucion())
+        ventana_a_configurar.title('pikWidget')
+        ventana_a_configurar.resizable(0,0)
+        ventana_a_configurar.iconbitmap('wm', r'media\favicon.ico')
 
-class establecer_preferencias_ventana():
-    def __init__(self, ventana_a_crear):
-        ventana_a_crear.geometry(user_preferences.get_resolucion())
-        ventana_a_crear.title('pikWidget')
-        ventana_a_crear.resizable(0,0)
-        ventana_a_crear.iconbitmap('wm', r'Proyecto\media\favicon.ico')
+class ConfigurarFrame:
+    def __init__(self, frame, porcentajealto, porcentajebajo):
+        with open(r'preferences\resolucion_usuario.txt', 'r') as archivo_resolucion:
+            resolucion = archivo_resolucion.readline()
+        ancho = int(resolucion[0:4])
+        alto = int(resolucion[5:9])
+        frame.config(width=porcentajealto/100*ancho, height=porcentajebajo/100*alto)
+
